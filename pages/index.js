4f9ade0f159eb970187a0ba4716c40be965f4c77
Igor Bayerl <bayerl.igor@gmail.com>
2021-02-23 11:46:46 -0300

    adding page sobre

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,14 @@
 import {useState} from 'react';
-
+import Link from 'next/link';
 
 function Home(){
     return (
         <div>
-            <h1>Home</h1>
+            <h1>Yoomy - Home</h1>
             <Contador/>
+            <Link href='/sobre'>
+                <a>sobre</a>
+            </Link>
         </div>
     )
 
---
+++ pages/sobre.js
@@ -0,0 +1,17 @@
+import Link from 'next/link';
+
+
+function Sobre(){
+    return (
+        <div>
+            <h1>Yoomy - Sobre</h1>
+            <Link href='/'>
+                <a>home</a>
+            </Link>
+        </div>
+    )
+
+}
+
+
+export default Sobre
