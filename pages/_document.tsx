0281e9cacaf9f240ae73523837f085a2eb7ef985
Igor Bayerl <bayerl.igor@gmail.com>
2021-02-27 23:35:07 -0300

    index google

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,6 +6,7 @@ export default class MyDocument extends Document {
             <Html>
                 <Head>
                     <meta name="name" content="Yoomy"/>
+                    <meta name="google-site-verification" content="rL0WQyAInIZEBhPV5C40ssChKzgnRDx3heD93UtoJfs" />
                     <meta name="description" content="Cardápio Digital para Restaurantes. Faça como os restaurantes mais modernos com as suluções Yoomy."/>
                     <link rel="preconnect" href="https://fonts.gstatic.com"/>
                     <link href="https://fonts.googleapis.com/css2?family=Nunito&display=swap" rel="stylesheet"/>
