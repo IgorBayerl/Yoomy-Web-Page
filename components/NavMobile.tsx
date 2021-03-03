e50cc36f58922c42e7851ec7e4b9789fe0443793
Igor Bayerl <bayerl.igor@gmail.com>
2021-03-03 11:17:15 -0300

    navBarIconsRotation

--- components/NavMobile.tsx
+++ components/NavMobile.tsx
@@ -9,24 +9,30 @@ function NavMobile(){
     function openNav() {
 
         document.getElementById("mySidenav").style.width = "450px";
+        document.getElementById("menuIcon").style.transform = "rotate(90deg)";
+        document.getElementById("closeXButton").style.transform = "rotate(180deg)";
+        document.getElementById("optionListDiv").style.fontSize = "20px"
     }
     
     function closeNav() {
         document.getElementById("mySidenav").style.width = "0";
+        document.getElementById("menuIcon").style.transform = "rotate(0deg)";
+        document.getElementById("closeXButton").style.transform = "rotate(0deg)";
+        document.getElementById("optionListDiv").style.fontSize = "0px"
     }
 
     return(
         <div className={styles.container}>
             <div className={styles.topBar}>
                 <Link href={'/'}>
-                    <a className={styles.openMenuButton} onClick={openNav}>
+                    <div id={'menuIcon'} className={styles.openMenuButton} onClick={openNav}>
                         <Image
                             src="/menu.svg"
                             alt="Menu"
                             width={50}
                             height={50}
                         />
-                    </a>
+                    </div>
                 </Link>
                 <div className={styles.yoomyHeaderContainer}>
                     <div className={styles.yoomyHeaderImage} >
@@ -49,16 +55,16 @@ function NavMobile(){
             </div>
             <div id={'mySidenav'} className={styles.sideNavContainer}>
                 <div className={styles.menuButtonContainer}>
-                    <a className={styles.menuButton} onClick={closeNav}>
+                    <div id={"closeXButton"} className={styles.menuButton} onClick={closeNav}>
                         <Image
                             src="/x.svg"
-                            alt="Yoomy"
+                            alt="X"
                             width={50}
                             height={50}
                         />
-                    </a>
+                    </div>
                 </div>
-                <div className={styles.optionsContainer}>
+                <div id={'optionListDiv'} className={styles.optionsContainer}>
                     <a href="">Aplicativo</a>
                     <a href="">Restaurante</a>
                     <a href="">Blog</a>
